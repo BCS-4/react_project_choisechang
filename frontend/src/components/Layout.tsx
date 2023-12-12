a4dac7a65d795c55cd4eb27fa52201fa549a4737
Sepang2 <sepang296@gmail.com>
2023-12-12 11:52:39 +0900

    Setting Layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 const Layout: FC = () => {
   return (
-    <div className="bg-red-100">
-      <header></header>
+    <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto">
+      <Header />
       <Outlet />
-      <footer></footer>
     </div>
   );
 };
