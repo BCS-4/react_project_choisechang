228b7c86e6b65ce966555305afcd36da34279520
Sepang2 <sepang296@gmail.com>
2023-12-14 11:52:50 +0900

    Infinite scroll

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,106 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { NftMetadata, OutletContext } from "../types";
+import axios from "axios";
+import NftCard from "../components/NftCard";
+
+const GET_AMOUNT = 8;
 
 const Home: FC = () => {
-  return <div className="bg-amber-100 grow">Home</div>;
+  const [searchTokenId, setSearchTokenId] = useState<number>(0);
+  const [totalNFT, setTotalNFT] = useState<number>(0);
+  const [metadataArray, setMetadataArray] = useState<NftMetadata[]>([]);
+
+  const { mintNftContract } = useOutletContext<OutletContext>();
+
+  const detectRef = useRef<HTMLDivElement>(null);
+  const observer = useRef<IntersectionObserver>();
+
+  const observe = () => {
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && metadataArray.length !== 0) {
+        getNFTs();
+      }
+    });
+
+    if (!detectRef.current) return;
+
+    observer.current.observe(detectRef.current);
+  };
+
+  const getTotalSupply = async () => {
+    try {
+      if (!mintNftContract) return;
+
+      const totalSupply = await mintNftContract.methods.totalSupply().call();
+
+      setSearchTokenId(Number(totalSupply));
+      setTotalNFT(Number(totalSupply));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getNFTs = async () => {
+    try {
+      if (!mintNftContract || searchTokenId <= 0) return;
+
+      let temp: NftMetadata[] = [];
+
+      for (let i = 0; i < GET_AMOUNT; i++) {
+        if (searchTokenId - i > 0) {
+          const metadataURI: string = await mintNftContract.methods
+            // @ts-expect-error
+            .tokenURI(searchTokenId - i)
+            .call();
+
+          const response = await axios.get(metadataURI);
+
+          temp.push({ ...response.data, tokenId: searchTokenId - i });
+        }
+      }
+      setSearchTokenId(searchTokenId - GET_AMOUNT);
+      setMetadataArray([...metadataArray, ...temp]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getTotalSupply();
+  }, [mintNftContract]);
+
+  useEffect(() => {
+    if (searchTokenId === 0) return;
+
+    getNFTs();
+  }, [totalNFT]);
+
+  useEffect(() => {
+    observe();
+
+    return () => observer.current?.disconnect();
+  }, [metadataArray]);
+
+  return (
+    <>
+      <div className="grow">
+        <ul className="p-8 grid grid-cols-2 gap-8">
+          {metadataArray?.map((v, i) => (
+            <NftCard
+              key={i}
+              image={v.image}
+              name={v.name}
+              tokenId={v.tokenId!}
+            />
+          ))}
+        </ul>
+      </div>
+      <div ref={detectRef} className="text-white py-4">
+        Detecting Area
+      </div>
+    </>
+  );
 };
 
 export default Home;
