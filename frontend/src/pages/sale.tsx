db78dc354388df93b2feddcf490c46fe89839cdb
Sepang2 <sepang296@gmail.com>
2023-12-15 14:31:32 +0900

    Update sale page

--- frontend/src/pages/sale.tsx
+++ frontend/src/pages/sale.tsx
@@ -1,7 +1,64 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { NftMetadata, OutletContext } from "../types";
+import axios from "axios";
+import SaleNftCard from "../components/SaleNftCard";
 
 const Sale: FC = () => {
-  return <div className="bg-blue-100 grow">Sale</div>;
+  const [metadataArray, setMetadataArray] = useState<NftMetadata[]>([]);
+
+  const { mintNftContract, saleNftContract } =
+    useOutletContext<OutletContext>();
+
+  const getSaleNFTs = async () => {
+    try {
+      const onSaleNFTs: bigint[] = await saleNftContract.methods
+        .getOnSaleNFTs()
+        .call();
+
+      let temp: NftMetadata[] = [];
+
+      for (let i = 0; i < onSaleNFTs.length; i++) {
+        const metadataURI: string = await mintNftContract.methods
+          // @ts-expect-error
+          .tokenURI(Number(onSaleNFTs[i]))
+          .call();
+
+        const response = await axios.get(metadataURI);
+
+        temp.push({ ...response.data, tokenId: Number(onSaleNFTs[i]) });
+      }
+      setMetadataArray(temp);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!saleNftContract) return;
+
+    getSaleNFTs();
+  }, [saleNftContract]);
+
+  return (
+    <div className="grow">
+      <div className="text-center py-8">
+        <h1 className="font-bold text-2xl">Sale NFTs</h1>
+      </div>
+      <ul className="p-8 grid grid-cols-2 gap-8">
+        {metadataArray?.map((v, i) => (
+          <SaleNftCard
+            key={i}
+            tokenId={v.tokenId!}
+            image={v.image}
+            name={v.name}
+            metadataArray={metadataArray}
+            setMetadataArray={setMetadataArray}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Sale;
